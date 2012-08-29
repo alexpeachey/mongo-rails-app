93ab809c6937b514efca1091394326f791eee0f8
Alex Peachey <alex.peachey@originate.com>
2012-08-29 12:48:07 -0700

    fix for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user
-    cu = super
-    if cu
-      UserDecorator.new cu
-    else
-      UserDecorator.new User.new
+    unless @decorated_current_user
+      cu = super
+      if cu
+        @decorated_current_user = UserDecorator.new cu
+      else
+        @decorated_current_user = UserDecorator.new User.new
+      end
     end
+
+    @decorated_current_user
   end
 end
